55838f67dea6930957556d6c0c142506073ed4f6
Endy Muhardin <endy.muhardin@gmail.com>
2012-10-24 10:26:14 +0700

    enable audit log untuk tabel produk

--- src/main/java/com/muhardin/endy/belajar/auditlog/domain/Produk.java
+++ src/main/java/com/muhardin/endy/belajar/auditlog/domain/Produk.java
@@ -9,8 +9,9 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import org.hibernate.envers.Audited;
 
-@Entity
+@Entity @Audited
 @Table(name="m_produk")
 public class Produk {
     @Id @GeneratedValue(strategy= GenerationType.AUTO)
